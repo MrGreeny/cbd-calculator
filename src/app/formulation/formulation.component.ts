4032b4f09ca04b16619e3699811826aa5b2ab086
MrGreeny <mrgreeny00@gmail.com>
2020-07-12 01:33:21 -0700

    formulation algorithm completed

--- src/app/formulation/formulation.component.ts
+++ src/app/formulation/formulation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CarrierOil } from '../../shared/models/carrierOil'
+import { renderFlagCheckIfStmt } from '@angular/compiler/src/render3/view/template';
 
 
 
@@ -15,8 +16,12 @@ export class FormulationComponent implements OnInit {
   carrierOilPrice: number;
   chosenExtractType: string;
   productQuantity: number;
+  productQuantityGram: number;
   cbdPrice: number;
   cbdQuantity: number;
+
+  costCarrierOil: number;
+  costCBD: number;
   
   //ID, Name, Price
   carrierOils: CarrierOil[] = [
@@ -32,9 +37,13 @@ export class FormulationComponent implements OnInit {
   ngOnInit(): void {
     this.cbdConcentration = 0.0;
     this.productQuantity = 0.0;
+    this.productQuantityGram = 0.0;
     this.carrierOilPrice = 0.0;
     this.cbdPrice = 0.0;
     this.cbdQuantity = 0.0;
+
+    this.costCarrierOil = 0.0;
+    this.costCBD= 0.0;
   }
 
   onChangeExtractType(event) {
@@ -43,35 +52,67 @@ export class FormulationComponent implements OnInit {
 
   onChangePercentConcentration(percentConcentration){
     this.cbdConcentration = percentConcentration;
-    this.cbdQuantity = this.productQuantity * (percentConcentration/100);
-
-    if (this.chosenExtractType === 'flower-extract') {
-      this.cbdPrice = 3.0;
-      
-    } else if ( this.chosenExtractType === 'flower-isolate') {
-      this.cbdPrice = 5.0;
-      
-    }
-    else {
-      //TODO: ERROR HANDLING
-    }
+    this.productQuantityGram = this.productQuantity * 1000;
+    this.cbdQuantity = this.productQuantityGram * (percentConcentration/100);
+    this.calculateCBDPrice()
     this.calculatePrice();
   }
 
   onChangeProductQuantity(productQuantity) {
+    //Kilograms in gramms
     this.productQuantity = productQuantity;
+    this.productQuantityGram = this.productQuantity * 1000;
+    this.cbdQuantity = this.productQuantityGram * (this.cbdConcentration/100);
+    this.calculateCBDPrice();
     this.calculatePrice();
   }
+
 
   onChangeCarrierOil(oil) {
     this.carrierOilPrice = oil.value;
     this.calculatePrice();
   }
 
+
+  calculateCBDPrice() {
+    let quantity = this.cbdQuantity
+    if (this.chosenExtractType === 'flower-extract') {
+
+      if (quantity > 0 && quantity < 5000) {
+        this.cbdPrice = 5.0;
+        this.price = this.cbdPrice * quantity * 0.33;
+      } else if (quantity >= 5000 && quantity < 10000) {
+        this.cbdPrice = 4.0;
+        this.price = this.cbdPrice * quantity * 0.33;
+      } else if (quantity >= 10000) {
+        this.cbdPrice = 3.0;
+        this.price = this.cbdPrice * quantity * 0.33;
+      }
+      //TODO: else & error handling
+
+    } else if ( this.chosenExtractType === 'isolate') {
+
+      console.log('isolate');
+      if (quantity > 0 && quantity < 5000) {
+        this.cbdPrice = 3.0;
+        this.price = this.cbdPrice * quantity;
+      } else if (quantity >= 5000 && quantity < 10000) {
+        this.cbdPrice = 2.5;
+        this.price = this.cbdPrice * quantity;
+      } else if (quantity >= 10000) {
+        this.cbdPrice = 2.0;
+        this.price = this.cbdPrice * quantity;
+      }      
+    }
+    else {
+      //TODO: ERROR HANDLING
+    }
+  }
+
   calculatePrice(){
-    let costCarrierOil = this.carrierOilPrice * this.productQuantity;
-    let costCBD = this.cbdPrice * this.cbdQuantity;
-    this.price = costCarrierOil + costCBD;
+    this.costCarrierOil = this.carrierOilPrice * this.productQuantity;
+    this.costCBD = this.cbdPrice * this.cbdQuantity;
+    this.price = this.costCarrierOil + this.costCBD;
   }
 }
 
